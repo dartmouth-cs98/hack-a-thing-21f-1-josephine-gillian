04bb8bc538ac536e339c03eca07ad5e7b24f4527
Gillian Yue <gillianyue1999@hotmail.com>
2021-09-18 22:36:52 -0400

    shadow texture

--- Assets/Scripts/GyroCamera.cs
+++ Assets/Scripts/GyroCamera.cs
@@ -12,6 +12,9 @@ public class GyroCamera : MonoBehaviour
     private Transform worldObj;
     private float startY;
 
+    [SerializeField]
+    private Transform zoomObj;
+
     void Start()
     {
         gyroSupported = SystemInfo.supportsGyroscope;
@@ -42,6 +45,21 @@ public class GyroCamera : MonoBehaviour
 
     void ResetGyroRotation()
     {
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(x, y));
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 500))
+        {
+            Vector3 hitPoint = hit.point;
+            hitPoint.y = 0;
+
+            float z = Vector3.Distance(Vector3.zero, hitPoint);
+            zoomObj.localPosition = new Vector3(0f, zoomObj.localPosition.y, Mathf.Clamp(z, 2f, 10f));
+        }
+
         startY = transform.eulerAngles.y;
         worldObj.rotation = Quaternion.Euler(0f, startY, 0f);
     }
